969d36ef63e55de9c33962f381e71476b9213688
Charles Koppelman-Milstein <ckoppelman@ngpvan.com>
2013-08-12 09:37:24 -0400

    added some real code in

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,36 @@
-javascript:(function(window, document) { myAlert = function (data) { alert(data.he); }; function makeScriptTag(refId) { var sefaria = document.createElement("script"); sefaria.type="application/javascript"; sefaria.src = "http://sefaria.org/api/texts/" + refId + "?context=0&commentary=0&callback=myAlert"; document.body.appendChild(sefaria); } window.addEventListener("input", function (eventData) { console.log(eventData); if(eventData.target.value && eventData.target.value.indexOf("Kohelet") !== -1) { makeScriptTag(eventData.target.value); } }); })(window, document);
+javascript:
+(function(window, document) {
+    var API = "http://sefaria.org/api/";
+    
+    myAlertHe = function (data) { alert(data.he); }; 
+    myAlertEn = function (data) { alert(data.en); }; 
+    function makeScriptTag(refId, lang) { 
+        var sefaria = document.createElement("script"); 
+        sefaria.type="application/javascript";
+        var callbackString = "myAlertHe";
+        if(lang && lang === "en") {
+            callbackString = "myAlertEn";
+        }
+        sefaria.src = API + "texts/" + refId + "?context=0&commentary=0&callback=myAlert";
+        document.body.appendChild(sefaria); 
+    }
+    var getAllSefariaSeforim = function () {
+        return ["Kohelet", "Shir haShirim"];
+    }
+    
+    window.addEventListener("input", function (eventData) { 
+        var seforim = getAllSeforim();
+        if(eventData.target.value) {
+            for(var i = 0; i < seforim.length; i++) {
+                var regex = new Regex("\\[\\s*sef:\\/\\/(" + seforim[i] + "(?=.*?))(?=/(en|he))?\\]", "mig");
+                while (true) {
+                    var match = regex.exec(textarea.value);
+                    if (!match) {
+                        break;
+                    }
+                    makeScriptTag(match[1], match[2]);
+                }
+            }
+        } 
+    }); 
+})(window, document);
