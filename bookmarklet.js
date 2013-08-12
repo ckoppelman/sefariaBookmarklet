c22d54c2204f24e617e88209d8139722a6921c78
Charles Koppelman-Milstein <ckoppelman@ngpvan.com>
2013-08-12 11:13:43 -0400

    jslint

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,36 +1,41 @@
 javascript:
-(function(window, document) {
-    var API = "http://sefaria.org/api/";
-    
-    myAlertHe = function (data) { alert(data.he); }; 
-    myAlertEn = function (data) { alert(data.en); }; 
-    function makeScriptTag(refId, lang) { 
-        var sefaria = document.createElement("script"); 
-        sefaria.type="application/javascript";
-        var callbackString = "myAlertHe";
-        if(lang && lang === "en") {
+(function (window, document) {
+    "use strict";
+	var API = "http://sefaria.org/api/",
+		getAllSefariaSeforim = function () {
+			return ["Kohelet", "Shir haShirim"];
+		};
+
+    window.myAlertHe = function (data) { window.alert(data.he); };
+    window.myAlertEn = function (data) { window.alert(data.en); };
+    function makeScriptTag(refId, lang) {
+        var sefaria = document.createElement("script"),
+			callbackString = "myAlertHe";
+        sefaria.type = "application/javascript";
+
+        if (lang && lang === "en") {
             callbackString = "myAlertEn";
         }
         sefaria.src = API + "texts/" + refId + "?context=0&commentary=0&callback=myAlert";
-        document.body.appendChild(sefaria); 
+        document.body.appendChild(sefaria);
     }
-    var getAllSefariaSeforim = function () {
-        return ["Kohelet", "Shir haShirim"];
-    }
-    
-    window.addEventListener("input", function (eventData) { 
-        var seforim = getAllSeforim();
-        if(eventData.target.value) {
-            for(var i = 0; i < seforim.length; i++) {
-                var regex = new Regex("\\[\\s*sef:\\/\\/(" + seforim[i] + "(?=.*?))(?=/(en|he))?\\]", "mig");
+
+    window.addEventListener("input", function (eventData) {
+        var seforim = getAllSefariaSeforim(),
+			i,
+			regex,
+			match;
+        if (eventData.target.value) {
+            for (i = 0; i < seforim.length; i += 1) {
+                regex = new RegExp("\\[\\s*sef:\\/\\/(" + seforim[i] + "(?=.*?))(?=/(en|he))?\\]", "mig");
                 while (true) {
-                    var match = regex.exec(textarea.value);
+                    match = regex.exec(eventData.target.value);
                     if (!match) {
                         break;
                     }
                     makeScriptTag(match[1], match[2]);
                 }
             }
-        } 
-    }); 
-})(window, document);
+        }
+    });
+}(window, document));
